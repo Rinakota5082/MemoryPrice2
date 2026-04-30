5f8214332ad9eeb418fd81e59e0938b949076587
Rinakota5082 <kula.rina.kota@yandex.ru>
2026-05-01 00:29:08 +0300

    part2

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class Door : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private XRGrabInteractable doorGrab;   // Ссылка на XR Grab Interactable ДВЕРИ
+    [SerializeField] private HingeJoint doorHinge;          // Hinge Joint ДВЕРИ
+
+    [Header("Visual & Audio")]
+    [SerializeField] private MeshRenderer doorRenderer;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip unlockSound;
+
+    public bool Point1 = false;
+    public bool Point2 = false;
+    public bool Point3 = false;
+    public bool Point4 = false;
+    public bool Point5 = false;
+    private bool isUnlocked = false;                        //счетчик
+
+    public void Update()
+    {
+        if (Point1 && Point2 && Point3 && Point4 && Point5) { Unlock(); }
+    }
+    void Start()
+    {
+        // Изначально ДВЕРЬ нельзя хватать
+        if (doorGrab != null)
+            doorGrab.enabled = false;
+
+        // Блокируем вращение двери
+        if (doorHinge != null)
+            doorHinge.useLimits = false;
+    }
+    
+    public void Unlock()
+    {
+        if (isUnlocked) return;
+        isUnlocked = true;
+
+        // . ДВЕРЬ теперь можно хватать!
+        if (doorGrab != null)
+            doorGrab.enabled = true;
+
+        // . Включаем лимиты вращения двери
+        if (doorHinge != null)
+            doorHinge.useLimits = true;
+
+        // . Звук
+        if (audioSource != null && unlockSound != null)
+            audioSource.PlayOneShot(unlockSound);
+
+        Debug.Log($"Door UNLOCKED! Now you can grab the DOOR.");
+    }
+}
